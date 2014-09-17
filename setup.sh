5add244ce61214db6b6c88c0fa27569211e4dc02
PolyFloyd <floyd@polyfloyd.net>
2014-09-17 09:31:41 +0200

    Added a setup script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+DIR=$(readlink -f $(dirname $0))
+
+enable_in_home() {
+	rm -rf $HOME/$2 2>/dev/null
+	ln -s "$DIR/user/$1" "$HOME/$2"
+}
+
+null_in_home() {
+	rm -rf $HOME/$1 2>/dev/null
+	ln -s /dev/null $HOME/$1
+}
+
+enable_in_home "bashrc"    ".bashrc"
+enable_in_home "profile"   ".profile"
+enable_in_home "profile"   ".xprofile"
+enable_in_home "dircolors" ".dircolors"
+enable_in_home "vim"       ".vim"
+enable_in_home "vim/vimrc" ".vimrc"
+enable_in_home "Xmodmap"   ".Xmodmap"
+
+null_in_home ".adobe"
+null_in_home ".macromedia"
