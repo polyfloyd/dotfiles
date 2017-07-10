d63f8388c6726b31e70461af2fdfe0aab3d6f22f
polyfloyd <floyd@polyfloyd.net>
2017-07-10 12:34:23 +0200

    Add configuration for Alacritty

--- setup.sh
+++ setup.sh
@@ -9,19 +9,20 @@ enable_in_home() {
 	ln -sfn "$DIR/user/$1" "$HOME/$2"
 }
 
-enable_in_home "Xmodmap"                       ".Xmodmap"
-enable_in_home "bashrc"                        ".bashrc"
-enable_in_home "dircolors/dircolors.ansi-dark" ".config/dircolors"
-enable_in_home "profile"                       ".profile"
-enable_in_home "vim"                           ".vim"
-enable_in_home "vim"                           ".config/nvim"
-enable_in_home "vimrc"                         ".vimrc"
+enable_in_home "Xmodmap"                        ".Xmodmap"
+enable_in_home "bashrc"                         ".bashrc"
+enable_in_home "dircolors/dircolors.ansi-dark"  ".config/dircolors"
+enable_in_home "profile"                        ".profile"
+enable_in_home "vim"                            ".vim"
+enable_in_home "vim"                            ".config/nvim"
+enable_in_home "vimrc"                          ".vimrc"
 
-enable_in_home "profile"                       ".xprofile"
-enable_in_home "config/i3/config"              ".config/i3/config"
-enable_in_home "config/compton.conf"           ".config/compton.conf"
-enable_in_home "config/terminator/config"      ".config/terminator/config"
-enable_in_home "mpdconf"                       ".mpdconf"
-enable_in_home "ncmpcpp/config"                ".ncmpcpp/config"
+enable_in_home "profile"                        ".xprofile"
+enable_in_home "config/i3/config"               ".config/i3/config"
+enable_in_home "config/compton.conf"            ".config/compton.conf"
+enable_in_home "config/terminator/config"       ".config/terminator/config"
+enable_in_home "config/alacritty/alacritty.yml" ".config/alacritty/alacritty.yml"
+enable_in_home "mpdconf"                        ".mpdconf"
+enable_in_home "ncmpcpp/config"                 ".ncmpcpp/config"
 
 vim +PluginInstall +qall
