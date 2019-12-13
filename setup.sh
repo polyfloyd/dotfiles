cd87253d7ae96231d11d44fcca33eb846df70638
polyfloyd <floyd@polyfloyd.net>
2019-12-13 18:24:15 +0100

    Install FZF from apt

--- setup.sh
+++ setup.sh
@@ -28,11 +28,4 @@ enable_in_home "ncmpcpp/config"                 ".ncmpcpp/config"
 enable_in_home "bin/brightness.sh"              ".bin/brightness.sh"
 enable_in_home "bin/lock.sh"                    ".bin/lock.sh"
 
-if [ ! -e ~/.fzf ]; then
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-else
-	cd ~/.fzf && git pull
-fi
-~/.fzf/install --key-bindings --no-completion --no-update-rc
-
 vim +PluginInstall +qall
