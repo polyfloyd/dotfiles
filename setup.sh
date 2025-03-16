1de5219a862cb1f57b2525a50de96738942ead07
polyfloyd <floyd@polyfloyd.net>
2025-03-16 15:02:43 +0100

    Stop provisioning .bashrc and profile

--- setup.sh
+++ setup.sh
@@ -9,9 +9,6 @@ enable_in_home() {
 	ln -sfn "$DIR/user/$1" "$HOME/$2"
 }
 
-enable_in_home "bashrc"                         ".bashrc"
-enable_in_home "dircolors/dircolors.ansi-dark"  ".config/dircolors"
-enable_in_home "profile"                        ".profile"
 enable_in_home "config/alacritty.toml"          ".config/alacritty.toml"
 enable_in_home "config/eww"                     ".config/eww"
 enable_in_home "config/sway"                    ".config/sway"
