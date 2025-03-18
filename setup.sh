095a78288f6087ca4adaf44f2a84af96350df282
polyfloyd <floyd@polyfloyd.net>
2025-03-18 11:44:47 +0100

    Stop provisioning neovim

--- setup.sh
+++ setup.sh
@@ -12,8 +12,4 @@ enable_in_home() {
 enable_in_home "config/alacritty.toml"          ".config/alacritty.toml"
 enable_in_home "config/eww"                     ".config/eww"
 enable_in_home "config/sway"                    ".config/sway"
-enable_in_home "nvim"                           ".config/nvim"
-
 enable_in_home "ncmpcpp/config"                 ".ncmpcpp/config"
-
-vim +PluginInstall +qall
