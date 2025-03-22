4dd5f5937c4377bb397d74e00be34d17716f099f
polyfloyd <floyd@polyfloyd.net>
2025-03-22 16:50:23 +0100

    Stop provisioning alacritty.toml

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ enable_in_home() {
 	ln -sfn "$DIR/user/$1" "$HOME/$2"
 }
 
-enable_in_home "config/alacritty.toml"          ".config/alacritty.toml"
 enable_in_home "config/eww"                     ".config/eww"
 enable_in_home "config/sway"                    ".config/sway"
 enable_in_home "ncmpcpp/config"                 ".ncmpcpp/config"
